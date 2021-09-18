7d97e2c1fb4da1ed43f8eebd773204289f85ade6
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-09-18 16:21:59 +0000

    lesson5

---
+++ Dev.lesson3.cpp
@@ -0,0 +1,97 @@
+/////////////////////////////////// start lesson 1 //////////////////////////////////////////
+
+
+//#include <stdio.h> // standar  input:kirish ,outtput:chiqrish chqirish
+//
+//int main (){
+//
+//printf("Hello world");
+//
+//return 0;
+//};
+
+
+// Compilyator bu bironbir dasturlash tiliga o'girib beradi  masalan || 010101
+// int funksiya turi.
+//printf ekranga chiqaruvchi funksya.
+
+
+
+
+///////////////////////////////////////////// start lesson 2 //////////////////////////////////////////////////
+
+
+//#include <stdio.h>
+//
+//
+//
+//int main (){
+//	int kelgan_ishchilar_soni;
+//	int kelmagan_ishchilar_soni;
+//	int jami_ishchilar_soni;
+//	
+//	
+//	
+//	printf("kelgan ishchilar sonini kiriting :");
+//	scanf("%d", &kelgan_ishchilar_soni );
+//	printf("kelmagan ishchilar sonini kiriting :");
+//	scanf("%d", &kelmagan_ishchilar_soni );
+//
+//	jami_ishchilar_soni = kelgan_ishchilar_soni + kelmagan_ishchilar_soni; 
+//	
+//	printf(" Jami ishchilar soni : %d", jami_ishchilar_soni);
+//	return  0;
+//}
+
+////////////////////////////////////////////// start lesson 2 ///////////////////////////////////////////////
+
+// & malumotlarni malum joyga saqlab beradi
+
+//#include <stdio.h>;
+//
+//int main (){
+//	
+//	int kelgan_ishchi;
+//	int kelmagan_ishchi;
+//	int jami;
+//			
+//	printf("kelgan ishchilar sonini kiriting : ");
+//	
+//	scanf("%d", &kelgan_ishchi);
+//	
+//	printf("kelmagan ishchilar soni:");
+//	
+//	scanf("%d", &kelmagan_ishchi);
+//	
+//	jami = kelgan_ishchi + kelmagan_ishchi;
+//	
+//	printf("jami ishchilar soni %d:", jami );
+//	
+//	
+//	return 0;
+//			
+//}
+
+// & malumotlarni malum joyga saqlab beradi
+// (scanf) bu terminalda javobni biz kirgizishimiz uchun qilingan funcksiya
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson5.c
@@ -0,0 +1,28 @@
+//#include <stdio.h>;
+//
+//
+//
+//#define mening_tug'ilgan_yilim'
+//int main (){
+//  float  my_Number = 12.1212;
+//	int your_Number = 12;
+//	char my_Name = A';
+// 
+// printf("int ning hajmi %d: \n ",sizeof(int));
+//  printf("char ning hajmi %d: \n ",sizeof(char));
+//   printf("Float ning hajmi %d: \n ",sizeof(float));
+// 
+// 
+// const int  my_day = 2003;
+// const float my_weight = 26;
+// 
+//	
+//printf("mening tug'ilgan yilim %d \n" , my_day);
+//printf("mening tug'ilgan kunim %.1f ", my_weight);
+//	return 0;
+//
+//};
+
+// float  qiymat "%f" nuqtali anniq bolmagan sonlarni oladi 12.1111
+// int qiymat %d anniq bolgan sonlarni oladi
+
